65da9a30777a1c2206696323597bb3d3fbd7ce45
Spandan Pradipkumar Chavda <113393534+thespandanchavda@users.noreply.github.com>
2023-03-26 19:45:10 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+
+
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <title>BARRIE ATMIYA YOUTHS</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container main">
+            <div class="row">
+                <div class="col-md-6 side-image">
+                    
+                </div>
+                <div class="col-md-6 right">
+                     <div class="input-box">
+                        <form action="https://formspree.io/f/xqkolzvw" method="POST">
+                        <header>Welcome to BARRIE ATMIYA YOUTHS</header>
+
+                        <div class="input-field">
+                            <input type="text" class="input" id="address" name="address"required autocomplete="off">
+                            <label for="email">Why Prayer is Important?</label>
+                        </div>
+
+                        <div class="input-field">
+                            <button type="submit">SUBMIT</button>
+                            
+                        </div>
+                        </form>
+                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
